fe7ddecb088dab688636a102fcc7984e63cabe03
prince suvagiya <108782673+princesuvagiya@users.noreply.github.com>
2023-03-10 10:53:22 +0530

    Livewire Contect form submit

---
+++ app/Http/Livewire/ContectForm.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Contact;
+
+class ContectForm extends Component
+{
+
+    public $firstName;
+    public $lastName;
+    public $email;
+    public $phone;
+    public $message;
+    public $successMessage;
+
+    protected $rules = [
+        'firstName' => 'required|',
+        'lastName' => 'required|min:4',
+        'email' => 'required|email',
+        'phone' => 'required',
+        'message' => 'required',
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submitForm()
+    {
+        $validatedData = $this->validate();
+
+
+        Contact::create([
+
+            'first_name' => $validatedData['firstName'],
+            'last_name' => $validatedData['lastName'],
+            'email' => $validatedData['email'],
+            'phone' => $validatedData['phone'],
+            'message' => $validatedData['message'],
+
+        ]);
+
+
+        $this->resetForm();
+
+        $this->successMessage = 'This Contect Form is successFully Submited!';
+    }
+
+
+
+    private function resetForm()
+    {
+
+        $this->firstName = null;
+        $this->lastName = null;
+        $this->email = null;
+        $this->phone = null;
+        $this->message = null;
+    }
+
+
+    public function render()
+    {
+        return view('livewire.contect-form');
+    }
+}
---
+++ resources/views/livewire/contect-form.blade.php
@@ -0,0 +1,184 @@
+<div>
+    <div class="relative isolate bg-gray-900">
+        <div class="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
+            <div class="relative px-6 pt-24 pb-20 sm:pt-32 lg:static lg:py-48 lg:px-8">
+                <div class="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
+                    <div class="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden ring-1 ring-white/5 lg:w-1/2">
+                        <svg class="absolute inset-0 h-full w-full stroke-gray-700 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+                            aria-hidden="true">
+                            <defs>
+                                <pattern id="54f88622-e7f8-4f1d-aaf9-c2f5e46dd1f2" width="200" height="200"
+                                    x="100%" y="-1" patternUnits="userSpaceOnUse">
+                                    <path d="M130 200V.5M.5 .5H200" fill="none" />
+                                </pattern>
+                            </defs>
+                            <svg x="100%" y="-1" class="overflow-visible fill-gray-800/20">
+                                <path d="M-470.5 0h201v201h-201Z" stroke-width="0" />
+                            </svg>
+                            <rect width="100%" height="100%" stroke-width="0"
+                                fill="url(#54f88622-e7f8-4f1d-aaf9-c2f5e46dd1f2)" />
+                        </svg>
+                        <svg class="absolute top-[calc(100%-13rem)] -left-56 w-[72.1875rem] transform-gpu blur-3xl lg:top-[calc(50%-7rem)] lg:left-[max(-14rem,calc(100%-59rem))]"
+                            viewBox="0 0 1155 678" aria-hidden="true">
+                            <path fill="url(#0a9a5302-e517-46c6-85f0-d826aa6a313e)" fill-opacity=".2"
+                                d="M317.219 159.025 203.852 0 0 239.659l317.219-80.634 204.172 286.402c1.307-132.337 45.083-346.658 209.733-145.248C936.936 551.942 882.053 772.234 1031.02 636.67c119.18-108.452 130.68-295.338 121.53-375.224L855 379l21.173-362.054-558.954 142.079Z" />
+                            <defs>
+                                <linearGradient id="0a9a5302-e517-46c6-85f0-d826aa6a313e" x1="1155.49" x2="-78.208"
+                                    y1="677.823" y2="203.355" gradientUnits="userSpaceOnUse">
+                                    <stop stop-color="#4F46E5" />
+                                    <stop offset="1" stop-color="#80CAFF" />
+                                </linearGradient>
+                            </defs>
+                        </svg>
+                    </div>
+                    <h2 class="text-3xl font-bold tracking-tight text-white">Get in touch</h2>
+                    <p class="mt-6 text-lg leading-8 text-gray-300">Proin volutpat consequat porttitor cras nullam
+                        gravida at. Orci molestie a eu arcu. Sed ut tincidunt integer elementum id sem. Arcu sed
+                        malesuada et magna.</p>
+                    <dl class="mt-10 space-y-4 text-base leading-7 text-gray-300">
+                        <div class="flex gap-x-4">
+                            <dt class="flex-none">
+                                <span class="sr-only">Telephone</span>
+                                <svg class="h-7 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                                </svg>
+                            </dt>
+                            <dd>545 Mavis Island<br>Chicago, IL 99191</dd>
+                        </div>
+                        <div class="flex gap-x-4">
+                            <dt class="flex-none">
+                                <span class="sr-only">Telephone</span>
+                                <svg class="h-7 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                                </svg>
+                            </dt>
+                            <dd><a class="hover:text-white" href="tel:+1 (555) 234-5678">+1 (555) 234-5678</a></dd>
+                        </div>
+                        <div class="flex gap-x-4">
+                            <dt class="flex-none">
+                                <span class="sr-only">Telephone</span>
+                                <svg class="h-7 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                                </svg>
+                            </dt>
+                            <dd><a class="hover:text-white" href="mailto:hello@example.com">hello@example.com</a></dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+
+            <form action="#" wire:submit.prevent="submitForm" method="POST"
+                class="px-6 pb-24 pt-20 sm:pb-32 lg:py-48 lg:px-8">
+
+
+                @if (!$successMessage == null)
+                    <div class="w-96 mx-auto mb-12 rounded-md bg-green-50 p-4">
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor"
+                                    aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="ml-3">
+                                <p class="text-sm font-medium text-green-800">{{ $successMessage }}</p>
+                            </div>
+                            <div class="ml-auto pl-3">
+                                <div class="-mx-1.5 -my-1.5">
+                                    <button type="button" wire:click="$set( 'successMessage' , null)"
+                                        class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50">
+                                        <span class="sr-only">Dismiss</span>
+                                        <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"
+                                            aria-hidden="true">
+                                            <path
+                                                d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+
+
+                <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
+                    <div class="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
+                        <div>
+                            <label for="first-name" class="block text-sm font-semibold leading-6 text-white">First
+                                name</label>
+                            <div class="mt-2.5">
+                                <input type="text" wire:model="firstName" name="first-name" id="first-name"
+                                    autocomplete="given-name"
+                                    class="block w-full rounded-md border-0 bg-white/5 py-2 px-3.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                                @error('firstName')
+                                    <p class="text-red-500 text-sm pt-2 text-medium">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+                        <div>
+                            <label for="last-name" class="block text-sm font-semibold leading-6 text-white">Last
+                                name</label>
+                            <div class="mt-2.5">
+                                <input type="text" wire:model="lastName" name="last-name" id="last-name"
+                                    autocomplete="family-name"
+                                    class="block w-full rounded-md border-0 bg-white/5 py-2 px-3.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                                @error('lastName')
+                                    <p class="text-red-500 text-sm pt-2 text-medium">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="sm:col-span-2">
+                            <label for="email"
+                                class="block text-sm font-semibold leading-6 text-white">Email</label>
+                            <div class="mt-2.5">
+                                <input type="email" wire:model="email" name="email" id="email"
+                                    autocomplete="email"
+                                    class="block w-full rounded-md border-0 bg-white/5 py-2 px-3.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                                @error('email')
+                                    <p class="text-red-500 text-sm pt-2 text-medium">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="sm:col-span-2">
+                            <label for="phone-number" class="block text-sm font-semibold leading-6 text-white">Phone
+                                number</label>
+                            <div class="mt-2.5">
+                                <input type="text" wire:model="phone" name="phone-number" id="phone-number"
+                                    autocomplete="tel"
+                                    class="block w-full rounded-md border-0 bg-white/5 py-2 px-3.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                                @error('phone')
+                                    <p class="text-red-500 text-sm pt-2 text-medium">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="sm:col-span-2">
+                            <label for="message"
+                                class="block text-sm font-semibold leading-6 text-white">Message</label>
+                            <div class="mt-2.5">
+                                <textarea type="text" name="message" wire:model="message" id="message" rows="4"
+                                    class="block w-full rounded-md border-0 bg-white/5 py-2 px-3.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"></textarea>
+                                @error('message')
+                                    <p class="text-red-500 text-sm pt-2 text-medium">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-8 flex justify-end">
+                        <button type="submit"
+                            class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Send
+                            message</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</div>
